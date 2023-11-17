973f2c8dcfee12679ff6ccc4e63cd77c637a3658
Ardcy <zhjgtao@gmail.com>
2023-11-17 15:31:20 +0800

    Simplify Lookaround internals

--- lookaround.py
+++ lookaround.py
@@ -3,45 +3,7 @@ from torch.optim.optimizer import Optimizer, required
 from torch import Tensor
 from typing import List, Optional
 import copy
-
-def sgd(params: List[Tensor],
-        d_p_list: List[Tensor],
-        momentum_buffer_list: List[Optional[Tensor]],
-        *,
-        weight_decay: float,
-        momentum: float,
-        lr: float,
-        dampening: float,
-        nesterov: bool,
-        maximize: bool):
-    r"""Functional API that performs SGD algorithm computation.
-
-    See :class:`~torch.optim.SGD` for details.
-    """
-
-    for i, param in enumerate(params):
-
-        d_p = d_p_list[i]
-        if weight_decay != 0:
-            d_p = d_p.add(param, alpha=weight_decay)
-
-        if momentum != 0:
-            buf = momentum_buffer_list[i]
-
-            if buf is None:
-                buf = torch.clone(d_p).detach()
-                momentum_buffer_list[i] = buf
-            else:
-                buf.mul_(momentum).add_(d_p, alpha=1 - dampening)
-
-            if nesterov:
-                d_p = d_p.add(buf, alpha=momentum)
-            else:
-                d_p = buf
-
-        alpha = lr if maximize else -lr
-        param.add_(d_p, alpha=alpha)
-       
+from torch.optim.sgd import sgd
 
 class Lookaround(Optimizer):
  
